bdc738c36fb7eac689fef59867516582079ef511
Puami <pourya.amiri.nasab@gmail.com>
2026-06-04 06:12:02 -0400

    Fix IPv6 routing with specific routes in entrypoint.sh

--- Assignment2/containerlab/bin/entrypoint.sh
+++ Assignment2/containerlab/bin/entrypoint.sh
@@ -7,16 +7,22 @@ if [ "$NODE_NAME" = "hs1" ]; then
     ip link set eth1 up
     ip addr add 10.0.1.2/24 dev eth1
     ip -6 addr add fc00:1::2/64 dev eth1
+    
     ip route add default via 10.0.1.1
-    ip -6 route add default via fc00:1::1
+   
+    ip -6 route add fc00:2::/64 via fc00:1::1
+    
     echo "[OK] hs1 configured."
 
 elif [ "$NODE_NAME" = "hs2" ]; then
     ip link set eth1 up
     ip addr add 10.0.2.2/24 dev eth1
     ip -6 addr add fc00:2::2/64 dev eth1
+    
     ip route add default via 10.0.2.1
-    ip -6 route add default via fc00:2::1
+
+    ip -6 route add fc00:1::/64 via fc00:2::1
+    
     echo "[OK] hs2 configured."
 
 elif [ "$NODE_NAME" = "rt1" ]; then
